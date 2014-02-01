9ac316f626932c062315a79327bf7beb23104703
Chris Blöcker <chrisbloecker@googlemail.com>
2014-02-01 15:30:41 +0100

    drone registration at queen

--- src/Hive.hs
+++ src/Hive.hs
@@ -1,20 +1,84 @@
-{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TemplateHaskell, DeriveGeneric, DeriveDataTypeable #-}
 
 module Main
   where
 
 import System.Environment (getArgs)
---import Control.Distributed.Process
+import Control.Distributed.Process
 
 import Control.Distributed.Process.Backend.SimpleLocalnet
 import Control.Distributed.Process.Closure
 import Control.Distributed.Process.Node hiding (newLocalNode)
 
+import GHC.Generics (Generic)
+
+import Data.Binary
+import Data.Typeable
+import Data.DeriveTH
+
+import Data.Set as Set
+
+type QueenSearchReply = Maybe ProcessId
+type Queen = ProcessId
+type Drone = ProcessId
+
+data RegisterMsg = Register { dronePid :: Drone }
+  deriving (Generic, Typeable, Show)
+
+data QueenState = QueenState { drones :: Set Drone }
+
+$(derive makeBinary ''RegisterMsg)
+
 remotable []
 
-startQueen = undefined
 
-startDrone = undefined
+mkRegisterMsg :: Drone -> RegisterMsg
+mkRegisterMsg pid = Register { dronePid = pid }
+
+
+searchQueen :: Backend -> Process QueenSearchReply
+searchQueen backend =
+  searchQueen' =<< liftIO (findPeers backend 2000)
+    where
+      searchQueen' :: [NodeId] -> Process QueenSearchReply
+      searchQueen' (peer:ps) = do
+        whereisRemoteAsync peer "queen"
+        WhereIsReply _name remoteWhereIs <- expect
+        case remoteWhereIs of
+          Just queenPid -> return (Just queenPid)
+          Nothing       -> searchQueen' ps
+      searchQueen' [] = return Nothing
+
+
+startQueen :: Backend -> Process ()
+startQueen backend = do
+  queenPid <- getSelfPid
+  register "queen" queenPid
+  say "Starting Queen..." >> serverLoop queenPid (QueenState { drones = empty})
+    where
+      serverLoop :: Queen -> QueenState -> Process ()
+      serverLoop queenPid state = do
+        liftIO . putStrLn $ "Registered drones: " ++ (show . size $ drones state)
+        receiveWait [ match (\(Register { dronePid = pid} ) -> do
+                        say $ "Drone registered at " ++ show pid
+                        serverLoop queenPid (state {drones = pid `insert` drones state}))
+                    , matchUnknown $ do 
+                        say "Unknown message received. Discarding..."
+                        serverLoop queenPid state
+                    ]
+
+
+startDrone :: Backend -> Process ()
+startDrone backend = do
+  dronePid <- getSelfPid
+  queen    <- searchQueen backend
+  case queen of
+    Just queenPid -> do
+      link queenPid
+      liftIO . putStrLn $ "Queen found at " ++ show queenPid
+      send queenPid $ mkRegisterMsg dronePid
+    Nothing -> liftIO . putStrLn $ "No Queen found..."
+
 
 main :: IO ()
 main = do
