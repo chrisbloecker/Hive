2167610144f304093763849db6c5840c7b1e3c7c
Christopher Blöcker <chrisbloecker@googlemail.com>
2014-09-25 19:50:43 +0200

    example usage in Arithmetic

--- src/Hive/Problem/Arithmetic.hs
+++ src/Hive/Problem/Arithmetic.hs
@@ -1,5 +1,21 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+-- | The hello world for interpreters combined with how to use the Hive processes algebra.
+--
+-- Using the Hive process algebra will look similar to this:
+--
+-- * define a data type that describes the computations to be carried out (in this case 'Expr')
+--
+-- * define basic (pure) functions that will be used to carry out computations on the defined data type
+-- or its components and wrap them into Cloud Haskell processes
+--
+-- * wrap the cloud haskell processes into Hive processes (typically by using the 'Hive.Process.Const'
+-- and 'Hive.Process.Simple' process creators from "Hive.Process")
+--
+-- * implement an interpreter function that takes a value of the data type that should be processed
+-- ('Expr') and creates a Hive process representing the desired computation (using the process combinators
+-- 'Hive.Process.Local', 'Hive.Process.Choice', 'Hive.Process.Sequence', 'Hive.Process.Parallel'
+-- , 'Hive.Process.Multilel' and 'Hive.Process.Loop' from "Hive.Process")
 module Hive.Problem.Arithmetic
   ( __remoteTable
   , Expr (..)
@@ -63,43 +79,79 @@ eval (Div x y) = eval x `div` eval y
 --
 -------------------------------------------------------------------------------
 
+-- | Takes an Int and creates a Cloud Haskell process which returns this Int
+--
+-- > val i = return i
 val :: Int -> CH.Process Int
 val i = return i
 
+-- | Takes a pair of Ints and creates a Cloud Haskell process which adds them
+--
+-- > add (x, y) = return (x + y)
 add :: (Int, Int) -> CH.Process Int
 add (x, y) = return (x + y)
 
+-- | Takes a pair of Ints and creates a Cloud Haskell process which subtracts the second from the first
+--
+-- > subtract (x, y) = return (x - y)
 subtract :: (Int, Int) -> CH.Process Int
 subtract (x, y) = return (x - y)
 
+-- | Takes a pair of Ints and creates a Cloud Haskell process which multiplies them
 multiply :: (Int, Int) -> CH.Process Int
 multiply (x, y) = return (x * y)
 
+-- | Takes a pair of Ints and creates a Cloud Haskell process which divides the first by the second
+--
+-- Yes, division by 0 is undefined.
+--
+-- > divide (_, 0) = undefined
+-- > divide (x, y) = return (x `div` y)
 divide :: (Int, Int) -> CH.Process Int
 divide (_, 0) = undefined
 divide (x, y) = return (x `div` y)
 
+-- | A SerializableDict for Int.
+-- This is neccessary for wrapping the previously defined Cloud Haskell processes into Hive processes.
+--
+-- > intDict = SerializableDict
 intDict :: SerializableDict Int
 intDict = SerializableDict
 
 -------------------------------------------------------------------------------
 
+-- We need to make all of these remotable and export the generated __remoteTable so we can spawn these on other nodes later.
 remotable ['val, 'add, 'subtract, 'multiply, 'divide, 'intDict]
 
 -------------------------------------------------------------------------------
 
+-- | Takes an Int and creates a Hive process which returns this Int based on 'val'
+--
+-- > valProcess i = mkConst $(mkStatic 'intDict) ($(mkClosure 'val) i)
 valProcess :: Int -> Process Int Int
 valProcess i = mkConst $(mkStatic 'intDict) ($(mkClosure 'val) i)
 
+-- | Creates a Hive process that expects a pair of Ints and adds them, based on 'add'
+--
+-- > addProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'add)
 addProcess :: Process (Int, Int) Int
 addProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'add)
 
+-- | Creates a Hive process that expects a pair of Ints and subtracts the second from the first, based on 'subtract'
+--
+-- > subtractProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'subtract)
 subtractProcess :: Process (Int, Int) Int
 subtractProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'subtract)
 
+-- | Creates a Hive process that expects a pair of Ints and multiplies them, based on 'multiply'
+--
+-- > multiplyProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'multiply)
 multiplyProcess :: Process (Int, Int) Int
 multiplyProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'multiply)
 
+-- | Creates a Hive process that expects a pair of Ints and divides the first by the second, based on 'divide'
+--
+-- > divideProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'divide)
 divideProcess :: Process (Int, Int) Int
 divideProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'divide)
 
@@ -107,6 +159,8 @@ divideProcess = mkSimple $(mkStatic 'intDict) $(mkClosure 'divide)
 -- interpretation of an Expr in form of a Process
 -------------------------------------------------------------------------------
 
+-- | The interpreter for 'Expr'. It translates a given 'Expr' into a Hive process and exploits the fact
+-- that for any operation the operands can be computed in parallel.
 interpret :: Expr -> Process Int Int
 interpret (Val i)   = valProcess i
 interpret (Add x y) = mkParallel (interpret x) (interpret y) addProcess
